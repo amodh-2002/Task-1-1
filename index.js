538ab926d66a45bb6ca5af158f47a353b2232ed5
unknown <snake@example.com>
2023-12-21 10:09:07 +0530

    1st Phase of Responsiveness

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let button = document.querySelector("#continue")
 let icon = document.querySelector("#icon-sun")
+let hamburger = document.querySelector(".hamburger")
+let navMenu = document.querySelector(".list-right ul")
 
 function validateForm() {
     let inputField = document.querySelector('input').value;
@@ -20,4 +22,16 @@ icon.addEventListener("click",()=>{
   }else{
     icon.src = "./sun.png"
   }
+})
+
+hamburger.addEventListener("click",()=>{
+  hamburger.classList.toggle("active")
+  navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll(".list-right ul li").forEach((n)=> {
+  n.addEventListener("click",()=>{
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+  })
 })
